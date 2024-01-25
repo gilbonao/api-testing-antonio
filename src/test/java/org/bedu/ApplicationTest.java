c94319d9add89aa25b4aa04cae6375520bf83944
Antonio Gilbon <gilbon.aburto.antonio@gmail.com>
2024-01-24 20:29:24 -0600

    Pruebas Unitarias sobre métodos de controller

--- src/test/java/org/bedu/ApplicationTest.java
+++ src/test/java/org/bedu/ApplicationTest.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ApplicationTest {
 
+	// Smoke test
+	// Prueba para verificar si se se ejecuta o carga la que estoy probando
 	@Test
 	void contextLoads() {
 	}
