d607c5fcfb18ab1965f06de2fe14aa20966ef773
Antonio Gilbon <gilbon.aburto.antonio@gmail.com>
2024-01-24 21:45:53 -0600

    Prueba nueva

---
+++ src/test/java/org/bedu/service/InterviewerServiceTest.java
@@ -0,0 +1,20 @@
+package org.bedu.service;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class InterviewerServiceTest {
+    @Autowired
+    private InterviewerService service;
+
+    @Test
+    @DisplayName("Service should be injected")
+    public void smokeTest() {
+        assertNotNull(service);
+    }
+}
